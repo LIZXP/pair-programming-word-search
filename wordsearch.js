b32bae299fa0bc256c256e4a2dbaaeca2a9e83ad
LIZXP <Peter.xli@outlook.com>
2022-06-18 20:55:20 -0600

    make new function to check the word vertically and add the test for that

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const verWordSearch = (lett, wor) => {
+  const newVerArr = [];
+  for (let i = 0; i < lett[0].length; i++) {
+    const colArr = [];
+    for (let j = 0; j < lett.length; j++) {
+      colArr.push(lett[j][i]);
+    }
+    newVerArr.push(colArr.join(""));
+    for (let l of newVerArr) {
+      if (l.includes(wor)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+module.exports = { wordSearch, verWordSearch };
